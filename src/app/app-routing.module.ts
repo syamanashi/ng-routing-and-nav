007fe8e21b7ef9bc64ae83321135fb41345fe06b
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-08-08 10:28:32 -0400

    Add CrisisList and HeroList components.
    Add styles from demo app: https://embed.plnkr.co/?show=preview
    
    $ ng g c components/crisis-list --spec=false
    
    $ ng g c components/hero-list --spec=false

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { HeroListComponent } from './components/hero-list/hero-list.component';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes, { enableTracing: true })], // { enableTracing: true } for debugging purposes only.
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { HeroListComponent } from './components/hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
