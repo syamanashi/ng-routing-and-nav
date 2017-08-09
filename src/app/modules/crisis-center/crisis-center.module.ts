ba2246dcde65d056601ff1541857fecd146573c8
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-08-09 18:16:28 -0400

    Child routing component
    - Add a crisis-center component.
    
    $ ng g c modules/crisis-center/crisis-center --module=modules/crisis-center --spec=false

--- src/app/modules/crisis-center/crisis-center.module.ts
+++ src/app/modules/crisis-center/crisis-center.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
 import { CrisisComponent } from './crisis/crisis.component';
 import { CrisesListComponent } from './crises-list/crises-list.component';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CrisisCenterRoutingModule
   ],
-  declarations: [CrisisComponent, CrisesListComponent]
+  declarations: [CrisisComponent, CrisesListComponent, CrisisCenterComponent]
 })
 export class CrisisCenterModule { }
---
+++ src/app/modules/crisis-center/crisis-center/crisis-center.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './crisis-center.component.html',
+  styleUrls: ['./crisis-center.component.scss']
+})
+export class CrisisCenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
